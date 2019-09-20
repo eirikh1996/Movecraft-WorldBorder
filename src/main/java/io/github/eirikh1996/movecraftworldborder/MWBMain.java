d271235c84f4128b470fccfc207a2007cca62c2b
eirikh1996 <eirikhetland1996@gmail.com>
2019-09-20 21:26:56 +0200

    Initial commit

---
+++ src/main/java/io/github/eirikh1996/movecraftworldborder/MWBMain.java
@@ -0,0 +1,111 @@
+package io.github.eirikh1996.movecraftworldborder;
+
+import com.wimbli.WorldBorder.BorderData;
+import com.wimbli.WorldBorder.Config;
+import com.wimbli.WorldBorder.WorldBorder;
+import net.countercraft.movecraft.Movecraft;
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.events.CraftRotateEvent;
+import net.countercraft.movecraft.events.CraftTranslateEvent;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MWBMain extends JavaPlugin implements Listener {
+    private static MWBMain instance;
+    private Movecraft movecraftPlugin;
+    private WorldBorder worldBorderPlugin;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        UpdateChecker.initialize();
+        MWBMain.getInstance().saveResource("localisation/mwblang_en.properties", false);
+        Settings.locale = getConfig().getString("locale", "en");
+        if (!I18nSupport.initialize()){
+            return;
+        }
+        Plugin movecraft = getServer().getPluginManager().getPlugin("Movecraft");
+        //Load up Movecraft
+        if (movecraft instanceof Movecraft){
+            getLogger().info(I18nSupport.getInternationalizedString("Startup - Movecraft found"));
+            movecraftPlugin = (Movecraft) movecraft;
+        }
+
+        //worldborder
+        Plugin wb = getServer().getPluginManager().getPlugin("WorldBorder");
+        if (wb instanceof WorldBorder){
+            getLogger().info(I18nSupport.getInternationalizedString("Startup - WorldBorder found"));
+            worldBorderPlugin = (WorldBorder) wb;
+        }
+
+        //If Movecraft was not found or is disabled, disable this plugin
+        if (movecraftPlugin == null || !movecraftPlugin.isEnabled()){
+            getLogger().severe(I18nSupport.getInternationalizedString("Startup - Movecraft not found or disabled"));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        //If WorldBorder was not found or is disabled, disable this plugin
+        if (worldBorderPlugin == null || !worldBorderPlugin.isEnabled()){
+            getLogger().severe(I18nSupport.getInternationalizedString("Startup - WorldBorder not found or disabled"));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getServer().getPluginManager().registerEvents(this, this);
+        UpdateChecker.getInstance().start();
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onCraftTranslate(CraftTranslateEvent event){
+        World world = event.getCraft().getW();
+        BorderData data = worldBorderPlugin.getWorldBorder(world.getName());
+        for (MovecraftLocation ml : event.getNewHitBox()){
+            if (event.getOldHitBox().contains(ml)){
+                continue;
+            }
+            if (data.insideBorder(ml.toBukkit(world))){
+                continue;
+            }
+            event.setFailMessage(I18nSupport.getInternationalizedString("Translation - Can't go past world border"));
+            event.setCancelled(true);
+            break;
+        }
+    }
+
+    @EventHandler
+    public void onCraftRotate(CraftRotateEvent event){
+        World world = event.getCraft().getW();
+        BorderData data = Config.Border(world.getName());
+        for (MovecraftLocation ml : event.getNewHitBox()){
+            if (event.getOldHitBox().contains(ml)){
+                continue;
+            }
+            if (data.insideBorder(ml.toBukkit(world))){
+                continue;
+            }
+            event.setFailMessage(I18nSupport.getInternationalizedString("Rotation - Can't go past world border"));
+            event.setCancelled(true);
+            break;
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        UpdateChecker.getInstance().sendUpdateMessage(event.getPlayer());
+    }
+
+    public static MWBMain getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/io/github/eirikh1996/movecraftworldborder/UpdateChecker.java
@@ -0,0 +1,90 @@
+package io.github.eirikh1996.movecraftworldborder;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UpdateChecker extends BukkitRunnable {
+    private static UpdateChecker instance;
+    private boolean running = false;
+
+    private UpdateChecker(){}
+    @Override
+    public void run() {
+        final double currentVersion = getCurrentVersion();
+        final double newVersion = checkUpdate(currentVersion);
+        MWBMain.getInstance().getLogger().info("Checking for updates");
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (newVersion > currentVersion){
+
+                    for (Player p : Bukkit.getOnlinePlayers()){
+                        sendUpdateMessage(p);
+                    }
+                    return;
+                }
+                MWBMain.getInstance().getLogger().info("You are up to date");
+            }
+        }.runTaskLaterAsynchronously(MWBMain.getInstance(), 100);
+    }
+
+    public static void initialize(){
+        instance = new UpdateChecker();
+    }
+
+    public static UpdateChecker getInstance() {
+        return instance;
+    }
+
+    public void start(){
+        if (running)
+            return;
+        runTaskTimerAsynchronously(MWBMain.getInstance(), 0, 100000000);
+        running = true;
+    }
+
+    public void sendUpdateMessage(Player player){
+        if (!player.hasPermission("mwb.update"))
+            return;
+        player.sendMessage(String.format("A new update of Movecraft-WorldBorder (v%f) is available.", checkUpdate(getCurrentVersion())));
+        player.sendMessage(String.format("You are currently on v%f", getCurrentVersion()));
+        player.sendMessage("Download at: ");
+    }
+
+    public double getCurrentVersion(){
+        return Double.parseDouble(MWBMain.getInstance().getDescription().getVersion());
+    }
+
+    public double checkUpdate(double currentVersion){
+        try {
+            URL url = new URL("https://servermods.forgesvc.net/servermods/files?projectids=342136");
+            URLConnection conn = url.openConnection();
+            conn.setReadTimeout(5000);
+            conn.addRequestProperty("User-Agent", "Movecraft-Factions Update Checker");
+            conn.setDoOutput(true);
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final String response = reader.readLine();
+            final JSONArray jsonArray = (JSONArray) JSONValue.parse(response);
+            if (jsonArray.size() == 0) {
+                MWBMain.getInstance().getLogger().warning("No files found, or Feed URL is bad.");
+                return currentVersion;
+            }
+            JSONObject jsonObject = (JSONObject) jsonArray.get(jsonArray.size() - 1);
+            String versionName = ((String) jsonObject.get("name"));
+            String newVersion = versionName.substring(versionName.lastIndexOf("v") + 1);
+            return Double.parseDouble(newVersion);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return currentVersion;
+        }
+    }
+}
